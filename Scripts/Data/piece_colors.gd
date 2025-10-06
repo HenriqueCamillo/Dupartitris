6a8249d9f4ef54c88aab715f9ce188ebeb179225
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2025-10-05 22:01:24 -0300

    Piece Colors as a separate resource

---
+++ Scripts/Data/piece_colors.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name PieceColors
+
+@export var colors_by_piece: Dictionary[PieceData, Color]
+
+func get_color(piece: PieceData) -> Color:
+	if !colors_by_piece.has(piece):
+		push_error("Trying to get color of a piece that isn't on PieceColors configuration.")
+		return Color.WHITE
+		
+	return colors_by_piece[piece]
--- Scripts/Gameplay/piece_spawner.gd
+++ Scripts/Gameplay/piece_spawner.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 @export var _piece_types: Array[PieceData]
 @export var _piece_template: PackedScene
-@export var _pieces_colors: Dictionary[PieceData, Color]
+@export var _piece_colors: PieceColors
 
 var _grid: TGrid
 
@@ -18,7 +18,9 @@ func spawn_piece() -> Piece:
 	var piece_type = _get_random_piece_type()
 	var piece = _piece_template.instantiate() as Piece
 	
-	piece.set_color(_pieces_colors[piece_type])
+	var color = _piece_colors.get_color(piece_type)
+	piece.set_color(color)
+	
 	piece.setup(piece_type, _grid)
 	piece.set_grid_position(_grid.spawn_position)
 	_grid.add_child(piece)
